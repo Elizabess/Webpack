cf16d14e13fc230f3b36b76826050eaefb99ad3c
Elizaveta <elizabess27@mail.ru>
2024-09-21 22:44:08 +0300

    Webpack

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+            {
+                test: /\.css$/, // Добавлено правило для обработки CSS
+                use: ['style-loader', 'css-loader'], // Используем style-loader и css-loader
+            },
+        ],
+    },
+    mode: 'development',
+};
+
+
